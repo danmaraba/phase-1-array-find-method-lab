d122ca6e92f1c551b88b9b757d0649a13b52aac9
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-13 07:59:46 +0300

    Done with this assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(items){
+    for (const record of items){
+    if (record.result==="W"){
+        return (record.year)
+    }
+}
+}
+record.find(superbowlWin)
